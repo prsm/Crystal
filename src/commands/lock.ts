316e786b677855df6a1a7140ac7263813543355f
Jannik66 <ja.noetzli@gmail.com>
2020-05-21 14:32:32 +0200

    🔒Add lock command to set userLimit of voice channel

---
+++ src/commands/lock.ts
@@ -0,0 +1,47 @@
+import { Message } from 'discord.js';
+
+import { juicepress } from '../bot';
+import { BotCommand } from '../customInterfaces';
+import config from '../config';
+
+export default class createRoleCommand implements BotCommand {
+    public information: BotCommand['information'] = {
+        id: 5,
+        name: 'lock',
+        category: 'Dynamic Channels',
+        description: 'Lock your voice channel to a specific amount of users. No limit or 0 unlocks the channel.',
+        argsRequired: false,
+        admin: false,
+        aliases: ['l'],
+        usage: 'lock [number]',
+        examples: ['lock 2', 'lock (to unlock)'],
+        showInHelp: true
+    }
+
+    constructor(private _botClient: juicepress) { }
+
+    public async execute(msg: Message, args: string[], prefix: string) {
+        // check if user is in a vocie channel
+        if (!msg.member.voice.channelID) {
+            msg.channel.send(':x: You aren\'t in a voice channel.');
+            return;
+        }
+        // check if user is in a dynamic voice channel
+        if (msg.member.voice.channel.parentID !== config.dynamicVoiceCategoryID) {
+            msg.channel.send(':x: Your voice channel is not a dynamic voice channel.');
+            return;
+        }
+        // if no arguments were provided, unlock the channel (set userlimit to 0)
+        if (!args[0]) {
+            msg.member.voice.channel.setUserLimit(0);
+            return;
+        }
+        // check if argument is a number and between 0 and 99
+        if (!args[0].match(/^\d+$/) || parseInt(args[0]) < 0 || parseInt(args[0]) > 99) {
+            msg.channel.send(':x: Userlimit has to be between 0 and 99');
+            return;
+        }
+        // set userlimit
+        msg.member.voice.channel.setUserLimit(parseInt(args[0]));
+    }
+}
