8adbe8e008f8a6a92ccf569738282122e0b7e54a
Jannik66 <ja.noetzli@gmail.com>
2020-05-22 20:09:48 +0200

    📈Add chart generation with Chart.js

---
+++ src/handlers/chartHandler.ts
@@ -0,0 +1,53 @@
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+import config from '../config';
+
+export class Charter {
+    private _window: jsdom.DOMWindow;
+
+    constructor() { }
+
+    public async draw(width: number, height: number, configuration: any) {
+        const dom = new jsdom.JSDOM(`
+        <html>
+            <body>
+                <div style="font-size:12; width:${width}; height:${height};">
+                    <canvas id="drawCanvas" width="${width}" height="${height}"></canvas>
+                </div>
+                <p id="base64Image"></p>
+            </body>
+        </html>`, { runScripts: 'dangerously' });
+        this._window = dom.window;
+
+        // Load the chart.js libary into the dom
+        const chartjs = fs.readFileSync(`${config.rootPath}/libraries/Chart.min.js`, { encoding: "utf-8" });
+        const scriptElement = this._window.document.createElement('script');
+        scriptElement.textContent = chartjs;
+        this._window.document.body.appendChild(scriptElement);
+
+        // generate chart
+        this._window.eval(`
+            canvas = document.getElementById('drawCanvas');
+            imageP = document.getElementById('base64Image');
+            
+            ctx = canvas.getContext('2d');
+
+            Chart.defaults.global.defaultFontSize=30;
+            Chart.defaults.global.defaultFontColor='#FFFFFF';
+
+            const generatedChart = new Chart(ctx, ${JSON.stringify(configuration)});
+            const base64 = generatedChart.toBase64Image();
+            imageP.innerHTML = base64;
+        `);
+
+        // save chart to file
+        await this.writeImageToFile('./database/chart.png');
+    }
+
+    // get base64 string and save it to a png image
+    public async writeImageToFile(filePath: string) {
+        fs.writeFileSync(filePath, this._window.document.getElementById('base64Image').innerHTML.replace(/^data:image\/png;base64,/, ''), { encoding: 'base64' });
+    }
+
+}
