b9bfd7532d9375bf2bcb276a0504d3121dfded34
Jannik66 <ja.noetzli@gmail.com>
2020-09-19 15:38:12 +0200

    :wheelchair: Update join & leave msgs

--- src/listeners/guildMemberListener.ts
+++ src/listeners/guildMemberListener.ts
@@ -13,14 +13,21 @@ export class GuildMemberListener {
         this._landingChannel = this._bot.getClient().channels.cache.get(config.landingChannelID) as TextChannel;
     }
 
+    public async evalGuildMemberAdd(member: GuildMember | PartialGuildMember) {
+        const embed = new MessageEmbed;
+        embed.setTitle(`Welcome to the PR1SM Discord Server`);
+        embed.setDescription(`${member.toString()}(\`${member.user.tag}\`) joined.\nPR1SM has now \`${member.guild.memberCount}\` members.`)
+        embed.setColor(0x28a745);
+        embed.setTimestamp(new Date());
+        this._landingChannel.send(embed);
+    }
+
     public async evalGuildMemberRemove(member: GuildMember | PartialGuildMember) {
-        if (member.roles.cache.get(config.memberRoleID)) {
-            const embed = new MessageEmbed;
-            embed.setTitle(`Member left`);
-            embed.setDescription(`\`${member.displayName}\`(${member.toString()}) left.\nWe are now \`${member.guild.memberCount}\` members.`)
-            embed.setColor(0xdc3545);
-            embed.setTimestamp(new Date());
-            this._landingChannel.send(embed);
-        }
+        const embed = new MessageEmbed;
+        embed.setTitle(`${member.displayName} left`);
+        embed.setDescription(`${member.toString()}(\`${member.user.tag}\`) left.\nPR1SM has now \`${member.guild.memberCount}\` members.`)
+        embed.setColor(0xdc3545);
+        embed.setTimestamp(new Date());
+        this._landingChannel.send(embed);
     }
 }
